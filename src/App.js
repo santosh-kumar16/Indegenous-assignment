b57b0d8b6d33aee2e3513ab34a41e4576f065b6f
santosh-kumar16 <gunturusantosh15@gmail.com>
2023-07-06 22:37:46 +0530

    modified App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+} from '@mui/material';
 
-function App() {
+
+
+const MyComponent = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://api.gyanibooks.com/library/get_dummy_notes/');
+        const jsonData = await response.json();
+        setData(jsonData);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <TableContainer component={Paper}>
+      <Table border="7">
+        <TableHead bgcolor="seablue" >
+          <TableRow >
+            <TableCell >ID</TableCell>
+            <TableCell>User</TableCell>
+            <TableCell>Title</TableCell>
+            <TableCell>Category</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {data.map((note) => (
+            <TableRow key={note.id}>
+              <TableCell bgcolor="pink" >{note.id}</TableCell>
+              <TableCell bgcolor="pink">{note.user}</TableCell>
+              <TableCell bgcolor="pink">{note.title}</TableCell>
+              <TableCell bgcolor="pink">{note.category}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
   );
-}
+};
 
-export default App;
+export default MyComponent;
